96fc47c3fd36cfc42ce586307778c40787a2f6f4
Bashar <basharaumary@gmail.com>
2020-12-14 16:34:02 +0100

    Compiled version of blockCipher

--- Crypt.hs
+++ Crypt.hs
@@ -4,17 +4,20 @@ import Data.List ()
 import Utils (bitStrToInt, chunksOf, octStrToBitStr, xorBits)
 
 -- Encryption function E
-encerypt :: String -> String -> String
-encerypt k m = executeRounds (k `xorBits` m) (keyScheduler k)
+encrypt :: String -> String -> String
+encrypt k m = executeRounds (k `xorBits` m) (drop 1 $ keyScheduler k)
   where
     executeRounds ct (k : ks) = executeRounds (roundFunction ct k) ks
     executeRounds ct [] = ct
 
 -- Decryption function D
 decrypt :: String -> String -> String
-decrypt = undefined
+decrypt k c = k `xorBits` executeRounds c (reverse $ drop 1 $ keyScheduler k)
+  where
+    executeRounds ct (k : ks) = executeRounds (inverseRoundFunction ct k) ks
+    executeRounds ct [] = ct
 
--- Phi
+-- Key octal permutation function Phi
 keyOctalPermutation :: String -> String
 keyOctalPermutation "000" = "011" -- Phi(0) = 3
 keyOctalPermutation "001" = "101" -- Phi(1) = 5
@@ -25,9 +28,9 @@ keyOctalPermutation "101" = "000" -- Phi(5) = 0
 keyOctalPermutation "110" = "110" -- Phi(6) = 6
 keyOctalPermutation "111" = "100" -- Phi(7) = 4
 
--- Key Scheduler H takes k0 and returns r-1 round keys [k1,...,k10]
+-- Key Scheduler H takes k0 and returns r+1 round keys [k0, k1,...,k10]
 keyScheduler :: String -> [String]
-keyScheduler k0 = keyScheduler' (nextRoundKey k0) (10 -1)
+keyScheduler k0 = keyScheduler' k0 10
   where
     keyScheduler' k 0 = [k]
     keyScheduler' k r = k : keyScheduler' (nextRoundKey k) (r -1)
@@ -41,19 +44,41 @@ splitToOctals :: String -> [String]
 splitToOctals = chunksOf 3
 
 -- Roundfunction R
+roundFunction :: String -> String -> String
 roundFunction ct k = k `xorBits` linearMap (applyPermutationMap ct)
 
+-- Inverse of round function R^-1
+inverseRoundFunction :: String -> String -> String
+inverseRoundFunction ct k = applyInversePermutationMap $ inverseLinearMap (k `xorBits` ct)
+
+-- S written in compressed octal notation
+s :: [String]
 s = chunksOf 2 "71057361762763417545466606045174643201774010235547241170253013435007524436142054031256340237162660356522175362425700156721337231"
 
--- inverted s [57, 18, 44, 40, 13, 1, 12, 33, 21, 26, 41, 30, 37, 58, 46, 52, 38, 60, 51, 22, 25, 28, 47, 5, 29, 63, 17, 61, 43, 49, 36, 45, 20, 7, 55, 31, 35, 9, 10, 24, 32, 14, 34, 53, 39, 23, 42, 56, 48, 3, 54, 6, 16, 50, 11, 59, 27, 0, 62, 2, 15, 8, 4, 19]
+-- Invesrse of S written in compressed octal notation
+inverteseS :: [String]
+inverteseS = chunksOf 2 "71225450150114412532513645725664467463263134570535772175536144552407673743111230401642654727527060036606206213733300760217100423"
 
 -- Substring permutation function S
 permutationMap :: String -> String
 permutationMap x = octStrToBitStr $ s !! bitStrToInt x
 
+-- Inverse substring permutation function S^-1
+inversePermutationMap :: String -> String
+inversePermutationMap x = octStrToBitStr $ inverteseS !! bitStrToInt x
+
 -- Application function for S on whole cyphertext
 applyPermutationMap :: String -> String
 applyPermutationMap x = concatMap permutationMap (chunksOf 6 x)
 
+-- Application function for S^-1 on whole cyphertext
+applyInversePermutationMap :: String -> String
+applyInversePermutationMap x = concatMap inversePermutationMap (chunksOf 6 x)
+
 -- Linear map function P
+linearMap :: [a] -> [a]
 linearMap [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17] = [b16, b14, b8, b2, b4, b11, b17, b5, b10, b3, b12, b15, b6, b9, b1, b7, b0, b13]
+
+-- Inverse linear map function P^-1
+inverseLinearMap :: [a] -> [a]
+inverseLinearMap [b16, b14, b8, b2, b4, b11, b17, b5, b10, b3, b12, b15, b6, b9, b1, b7, b0, b13] = [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17]
--- Main.hs
+++ Main.hs
@@ -1,7 +1,17 @@
 module Main where
 
-import Crypt ()
+import Crypt (decrypt, encrypt)
+import Data.List ()
+import System.Environment
 import Utils ()
 
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  case args of
+    ("-e" : k : m : _) ->
+      putStrLn $ encrypt k m
+    ("-d" : k : c : _) ->
+      putStrLn $ decrypt k c
+    _ ->
+      putStrLn "Usage: -e <18bit key> <18bit message> | -d <18bit key> <18bit cyphertext>"
